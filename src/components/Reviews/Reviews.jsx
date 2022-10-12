7d7dd3b5f51929261eeb24a9cdcd8cf153002c4d
gdutchak <galyna.dutchak99@gmail.com>
2022-10-13 00:15:20 +0300

    update reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,7 @@
 import { List } from "components/Cast/Cast.styled";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import fetchInfoReview from "utils/fetchInfoReview";
 
 const Review = () => {
@@ -10,21 +10,19 @@ const Review = () => {
 
     useEffect(() => {
         fetchInfoReview(id).then(r => setReview(r.results))
-    }, [])
+    }, [id])
 
     if (!review || review.length === 0) {
         return <p>This film hasn`t feedback yet. You can be first!</p>;
     } else {
-        {
-            return <List>
-                {review.map(({ author, content, id }) => {
-                    return <li key={id}>
-                        <h4>{author}</h4>
-                        <p>{content}</p>
-                    </li>
-                })}
-            </List>
-        }
+        return <List>
+            {review.map(({ author, content, id }) => {
+                return <li key={id}>
+                    <h4>{author}</h4>
+                    <p>{content}</p>
+                </li>
+            })}
+        </List>
     }
 }
 
