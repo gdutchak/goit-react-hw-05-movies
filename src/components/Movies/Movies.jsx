0b51e2806fd5cbf5299cd85e19eaffdc55c31aca
gdutchak <galyna.dutchak99@gmail.com>
2022-10-13 08:09:17 +0300

    update movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,13 +1,12 @@
 import { Suspense, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useSearchParams } from "react-router-dom";
 import fetchFilmsCoreWords from "utils/fetchFilmsCoreWords";
-import { ButtonInp, Input, LinkEl, List } from "./Movies.styled";
+import { ButtonInp, Input, LinkEl } from "./Movies.styled";
 
 const FetchFilmsCoreWords = () => {
-    const [name, setName] = useState('');
     const [collection, setCollection] = useState(null);
-    // const [search, setSearch] = useSearchParams();
-    // const name = search.get('search');
+    const [search, setSearch] = useSearchParams();
+    const name = search.get('search') ?? '';
     const location = useLocation();
 
     const findFilms = () => {
@@ -23,16 +22,13 @@ const FetchFilmsCoreWords = () => {
 
     return (
         <>
-            <Input name="name" onInput={e => setName(e.target.value)}></Input>
+            <Input name="name" onInput={e => setSearch(search !== '' ? { search: e.target.value } : {})}></Input>
             <ButtonInp type="submit" onClick={findFilms}>Search</ButtonInp>
-            <List>
-                {collection ? collection.map(({ id, original_title }) =>
-                    <LinkEl key={id} state={{ from: location }} to={`${id}`}>{original_title}</LinkEl>) : null}
-            </List>
+            {collection ? collection.map(({ id, original_title }) =>
+                <LinkEl key={id} state={{ from: location }} to={`${id}`}>{original_title}</LinkEl>) : null}
             <Suspense fallback={null}><Outlet /></Suspense>
         </>
     )
 }
 
 export default FetchFilmsCoreWords;
-// id, 
